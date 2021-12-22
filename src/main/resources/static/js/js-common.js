78e4e585b90cdb6648e8fa65943b8a729d996d62
jaeuk <iopqwas@kacelab.com>
2021-12-23 02:32:32 +0900

    관리자관리 비밀번호 변경하는중..엑셀다운로드 하는 중.

--- src/main/resources/static/js/js-common.js
+++ src/main/resources/static/js/js-common.js
@@ -12,3 +12,22 @@ function toStringByFormatting(source, delimiter = '-') {
     const day = leftPad(source.getDate());
     return [year, month, day].join(delimiter);
 }
+
+function toStringByFormattingHm(source, delimiter = '-') {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+    const hour = leftPad(source.getHours());
+    const minutes = leftPad(source.getMinutes());
+    return [year, month, day].join(delimiter)+" "+[hour, minutes].join(delimiter);
+}
+
+function toStringByFormattingHms(source, delimiter = '-') {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+    const hour = leftPad(source.getHours());
+    const minutes = leftPad(source.getMinutes());
+    const second = leftPad(source.getSeconds());
+    return [year, month, day].join(delimiter)+" "+[hour, minutes, second].join(delimiter);
+}
