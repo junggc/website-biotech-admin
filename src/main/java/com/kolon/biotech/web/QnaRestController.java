4cee5ba60329519a5ab5000b28b23ff7f5e86d11
jaeuk <iopqwas@kacelab.com>
2021-12-30 00:27:05 +0900

    일부반영

--- src/main/java/com/kolon/biotech/web/QnaRestController.java
+++ src/main/java/com/kolon/biotech/web/QnaRestController.java
@@ -1,12 +1,15 @@
 package com.kolon.biotech.web;
 
+import com.kolon.biotech.domain.notice.Noticefile;
 import com.kolon.biotech.domain.qna.Qna;
 import com.kolon.biotech.domain.subsidiary.Subsidiary;
 import com.kolon.biotech.service.QnaService;
+import com.kolon.biotech.web.dto.DownloadView;
 import com.kolon.biotech.web.dto.ResultJsonPagingDto;
 import com.kolon.biotech.web.dto.SearchDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.data.domain.Page;
@@ -17,6 +20,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.LocaleResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -67,5 +73,29 @@ public class QnaRestController {
         }
 
         return dto;
+    }
+
+    @GetMapping("/download")
+    public void download(@RequestParam(value = "id") Integer id, HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+        Qna qna = qnaService.getInfo(id);
+
+        if(qna != null && !ObjectUtils.isEmpty(qna) && qna.getId() > 0){
+
+            try{
+
+                DownloadView fileDown = new DownloadView(); //파일다운로드 객체생성
+                fileDown.filDown(request, response, qna.getFilePath() , qna.getFileName()); //파일다운로드
+
+            }catch(Exception e){
+                log.error("filedownerror",e.getMessage());
+                throw new Exception();
+            }
+
+        }else{
+            log.error("filedownerror","null");
+            throw new Exception();
+        }
+
     }
 }
