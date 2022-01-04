65c996ff9772c144c333046f594a359dbd7ca01b
Jeong yong hoon <jyh.dev@gmail.com>
2022-01-04 17:17:07 +0900

    정규식 수정

--- src/main/java/com/kolon/biotech/config/auth/XssRequest.java
+++ src/main/java/com/kolon/biotech/config/auth/XssRequest.java
@@ -180,8 +180,8 @@ public class XssRequest extends HttpServletRequestWrapper {
 
             rslt = rslt.replaceAll("(?i)cookie", "cook1e");
             rslt = rslt.replaceAll("(?i)document", "d0cument");
-            rslt = rslt.replaceAll("(?i)script", "scr1pt");
-            rslt = rslt.replaceAll("(?i)alert", "a1ert");
+            rslt = rslt.replaceAll("(?i)<script", "&lt;script");
+            rslt = rslt.replaceAll("(?i)<alert", "&lt;alert");
             //#&;
 
         }
