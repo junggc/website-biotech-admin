4fc4a0467b7cd6992e1449ea62ce602c38b5ce98
kimajeuk <iopqwas@naver.com>
2022-01-10 14:54:25 +0900

    3자조치

--- src/main/resources/static/js/jquery-common.js
+++ src/main/resources/static/js/jquery-common.js
@@ -80,4 +80,11 @@
         }
     });
 
+
+    numberMaxLength = function (e){
+        if(e.value.length > e.maxLength){
+            e.value = e.value.slice(0, e.maxLength);
+        }
+    }
+
 })(jQuery);
